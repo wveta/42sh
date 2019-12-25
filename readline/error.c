93b6908ac6873d8e39f8cc243415d5af14626160
Theron Haley <thaley@mi-a3.21-school.ru>
2019-12-25 19:38:13 +0300

    more small fixes

--- readline/error.c
+++ readline/error.c
@@ -1,40 +1,29 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_strnjoin.c                                      :+:      :+:    :+:   */
+/*   error.c                                            :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: thaley <thaley@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2019/12/08 01:47:55 by thaley            #+#    #+#             */
-/*   Updated: 2019/12/25 19:17:53 by thaley           ###   ########.fr       */
+/*   Created: 2019/12/25 19:30:48 by thaley            #+#    #+#             */
+/*   Updated: 2019/12/25 19:31:24 by thaley           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "libft.h"
+#include "readline.h"
 
-char		*ft_strnjoin(char *s1, char *s2, int len)
+void		bad_fd_error(int fd, char *tmp)
 {
-	int		i;
-	int		j;
-	char	*ret;
+	if (fd == 1)
+		ft_print_msg(" : Error open STDOUT: ", tmp);
+	else
+		ft_print_msg(" : Error open STDERR: ", tmp);
+	exit(EXIT_FAILURE);
+}
 
-	i = 0;
-	j = 0;
-	if (!(ret = (char *)malloc(sizeof(char) * (len + 1))))
-		return (NULL);
-	if (s1)
-	{
-		while (s1[i] && i < len)
-		{
-			ret[i] = s1[i];
-			i++;
-		}
-	}
-	if (s2)
-	{
-		while (s2[j] && i < len)
-			ret[i++] = s2[j++];
-	}
-	ret[i] = '\0';
-	return (ret);
+void		mall_check(void)
+{
+	go_end_pos();
+	ft_putstr_fd("ERROR: Cannot allocate memory.", STDERR_FILENO);
+	return_func("\0", "exit");
 }
