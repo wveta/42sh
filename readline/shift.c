020788bb3753e5eecdcaa4303f2484ae0fccac2f
Theron Haley <thaley@mi-a3.21-school.ru>
2019-12-26 18:05:20 +0300

    something -_-

--- readline/shift.c
+++ readline/shift.c
@@ -6,7 +6,7 @@
 /*   By: thaley <thaley@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/12/23 15:42:43 by thaley            #+#    #+#             */
-/*   Updated: 2019/12/23 19:41:49 by thaley           ###   ########.fr       */
+/*   Updated: 2019/12/26 18:04:27 by thaley           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,22 +17,22 @@ char		*check_shift(char *str)
 	if (!(ft_strcmp(str, SHIFT_LFT)))
 	{
 		ft_bzero(str, 20);
-		str[0] = 'D';
+		str = ft_strcpy(str, ";2D");
 	}
 	else if (!(ft_strcmp(str, SHIFT_RGT)))
 	{
 		ft_bzero(str, 20);
-		str[0] = 'C';
+		str = ft_strcpy(str, ";2C");
 	}
 	else if (!(ft_strcmp(str, SHIFT_UP)))
 	{
 		ft_bzero(str, 20);
-		str[0] = 'A';
+		str = ft_strcpy(str, ";2A");
 	}
 	else if (!(ft_strcmp(str, SHIFT_DO)))
 	{
 		ft_bzero(str, 20);
-		str[0] = 'B';
+		str = ft_strcpy(str, ";2B");
 	}
 	else if (str[0] == '\e')
 		ft_bzero(str, 20);
