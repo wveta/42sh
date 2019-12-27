e7cea56f3e44f93a166f89c1828ebf91772e5654
Theron Haley <thaley@mi-a3.21-school.ru>
2019-12-27 12:16:36 +0300

    sigwinsize

--- readline/signal.c
+++ readline/signal.c
@@ -6,7 +6,7 @@
 /*   By: thaley <thaley@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/11/27 17:48:56 by thaley            #+#    #+#             */
-/*   Updated: 2019/12/25 19:27:34 by thaley           ###   ########.fr       */
+/*   Updated: 2019/12/27 12:14:55 by thaley           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,22 +14,8 @@
 
 void		ft_signal_win_size(int signo)
 {
-	char	tmp[MAX_CMDS];
-
 	(void)signo;
-	ft_bzero(tmp, MAX_CMDS);
-	ft_strcpy(tmp, g_input->input);
-	ft_bzero(g_input->input, MAX_CMDS);
 	ioctl(STDIN_FILENO, TIOCGWINSZ, &g_input->ws);
-	while (g_input->multiline.pos > 0)
-	{
-		tputs(tgetstr("up", NULL), 1, putint);
-		g_input->multiline.pos--;
-	}
-	tputs(tgetstr("cr", NULL), 1, putint);
-	tputs(tgetstr("cd", NULL), 1, putint);
-	ft_putstr_fd(g_input->prompt, STDERR_FILENO);
-	g_input->curs_pos = g_input->prompt_len;
-	g_input->input_len = 0;
-	print(tmp);
+	null_multiline();
+	count_lines();
 }
